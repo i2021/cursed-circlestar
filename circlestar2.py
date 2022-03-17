10ef75c3e17e51479d971e41f3b5ef0a6162ed46
2021_ <72861096+i2021@users.noreply.github.com>
2022-03-17 08:11:52 +0100

    deshitting shitcode

--- circlestar2.py
+++ circlestar2.py
@@ -4,8 +4,9 @@ import math
 import matplotlib.pyplot as plt
 
 # declaring the radii of small and big circles
-smallR = 3
-bigR = 24
+# used to be 3, 24
+smallR = float(input("enter the radius of the inner circle: "))
+bigR = float(input("enter the radius of the outer circle: "))
 
 # find the perimeter of circle
 perimeterSmall = 2 * math.pi * smallR
@@ -14,13 +15,16 @@ perimeterSmall = 2 * math.pi * smallR
 # stop = 100
 # step = 1
 # I added for loop for some reason...
-for i in range(1, 100000, 100):
+for i in range(1, 10000, 10):
     # find the arc length of one triangle
     triangles = i
     arcLen = perimeterSmall / triangles
 
+    # subdivide the crystal
+    halfArcLen = arcLen / 2
+
     # find the angle between b & c (smallR & bigR)
-    theta = arcLen / ((math.pi / 180) / smallR)
+    theta = halfArcLen / ((math.pi / 180) / smallR)
 
     # find the missing side
     sideA = math.sqrt((smallR ** 2) + (bigR ** 2) - (2 * smallR * bigR) * math.cos(theta))
